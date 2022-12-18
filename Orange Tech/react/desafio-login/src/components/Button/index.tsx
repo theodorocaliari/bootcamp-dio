f56c8acf91d025a840ae0e6e8118d918167e732f
theodorocaliari <theodorocaliari@gmail.com>
2022-12-18 19:51:43 -0300

    Desafio tela login

---
+++ Orange Tech/react/desafio-login/src/components/Button/index.tsx
@@ -0,0 +1,12 @@
+import { ButtonContainer } from "./styles";
+import { IButtonProps } from "./types";
+
+const Button = ({ title, disabled, onClick }: IButtonProps) => {
+  return <>
+    <ButtonContainer disabled={disabled} onClick={onClick}>
+      {title}
+    </ButtonContainer>
+  </>
+};
+
+export default Button;
